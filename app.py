bbd5517fd7404c5cabeb5ff20ae736337740a32d
WIjarn Bangmo <draft.sama@gmail.com>
2023-04-25 09:24:39 +0700

    [add]line webhook

--- app.py
+++ app.py
@@ -1,9 +1,87 @@
-from flask import Flask, jsonify, request
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from flask import Flask, jsonify, request, abort
 from waitress import serve
+from keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps
+import numpy as np
+from io import BytesIO
+import base64
+from dotenv import load_dotenv
+import os
+
+# Load variables from .env file into environment
+load_dotenv()
+
+
+PORT = int(os.getenv('PORT'))
+CHANNEL_ACCESS_TOKEN = os.getenv('CHANNEL_ACCESS_TOKEN')
+CHANNEL_SECRET = os.getenv('CHANNEL_SECRET')
+MODE = os.getenv('MODE')
 
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = load_model("keras_Model.h5", compile=False)
+
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
+
+
+def test():
+    image = Image.open('input.png').convert('RGB')
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+    image_array = np.asarray(image)
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+    data[0] = normalized_image_array
+    prediction = model.predict(data)
+
+    print(class_names, prediction)
+    image.close()
+
+
+# test()
+
+
+def load_image_from_base64(base64_string):
+    img_data = base64.b64decode(base64_string)
+    img = Image.open(BytesIO(img_data))
+    return img
+
+
+@app.route('/webhook', methods=['POST'])
+def lineWebhook():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
 
 
 @app.route('/get', methods=['GET'])
@@ -11,11 +89,36 @@ def hello_world():
     return jsonify([{"id": 1, "name": "Draft"}, {"id": 2, "name": "Tester"}])
 
 
-mode = "pro"
+@app.route('/check_image', methods=['POST'])
+def upload_image():
+    base64 = request.json['image']
+    image = load_image_from_base64(base64).convert('RGB')
+    # file_path = "image.png"
+    # save_image_from_base64(base64, file_path)
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+    image_array = np.asarray(image)
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+    data[0] = normalized_image_array
+
+    # Predicts the model
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    result = {"class": class_name[2:].strip(
+    ), "score": float(confidence_score)}
+
+    image.close()
+    base64 = ''
+
+    return jsonify(result)
 
 
 if __name__ == '__main__':
-    if mode == "dev":
-        app.run(host='0.0.0.0', port=50100, debug=True)
+    if MODE == "development":
+        app.run(host='0.0.0.0', port=PORT, debug=True)
     else:
-        serve(app, host='0.0.0.0', port=50100, threads=1)
+        serve(app, host='0.0.0.0', port=PORT, threads=2)
