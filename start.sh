e7f202e6dbbc851f9cb41933661534d3dbb3845c
WIjarn Bangmo <draft.sama@gmail.com>
2023-06-21 17:39:30 +0700

    test3

--- start.sh
+++ start.sh
@@ -3,6 +3,5 @@ DIR=$(dirname "$0")
 
 CERT_FILE=/home/ubuntu/ssl_certificate/certificate.pem
 KEY_FILE=/home/ubuntu/ssl_certificate/privatekey.pem
-
 $DIR/venv/bin/gunicorn --bind 0.0.0.0:5000 --certfile=$CERT_FILE --keyfile=$KEY_FILE app:app
 
