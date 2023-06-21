e0939b22b0693963883ecb9054d1385323743662
WIjarn Bangmo <draft.sama@gmail.com>
2023-06-21 17:48:16 +0700

    test3

--- start.sh
+++ start.sh
@@ -3,5 +3,7 @@ DIR=$(dirname "$0")
 
 CERT_FILE=/home/ubuntu/ssl_certificate/certificate.pem
 KEY_FILE=/home/ubuntu/ssl_certificate/privatekey.pem
+export MODE=production
+export OPENAI_API_KEY=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
 $DIR/venv/bin/gunicorn --bind 0.0.0.0:5000 --certfile=$CERT_FILE --keyfile=$KEY_FILE app:app
 
