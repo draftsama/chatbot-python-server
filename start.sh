761fbf5674740c409697b83bf042cbf46ab689f0
WIjarn Bangmo <draft.sama@gmail.com>
2023-06-21 20:41:41 +0700

    test api

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ CERT_FILE=/home/ubuntu/ssl_certificate/certificate.pem
 KEY_FILE=/home/ubuntu/ssl_certificate/privatekey.pem
 export MODE=production
 export OPENAI_API_KEY=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
-$DIR/venv/bin/gunicorn --bind 0.0.0.0:5000 --certfile=$CERT_FILE --keyfile=$KEY_FILE app:app
+$DIR/venv/bin/gunicorn -w 4 -b 0.0.0.0:5000 --certfile=$CERT_FILE --keyfile=$KEY_FILE app:app
 
