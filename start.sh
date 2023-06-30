120983c43c9e433a7c6990bbce1a1691dfcdba32
WIjarn Bangmo <draft.sama@gmail.com>
2023-06-30 14:05:00 +0700

    [update]

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 export OPENAI_API_KEY=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
-
 DIR=$(dirname "$0")
-
 export MODE=production
 
 # Change directory to project directory
@@ -14,3 +12,4 @@ pkill -f "$DIR/venv/bin/gunicorn -w 4 -b 0.0.0.0:$PORT app:app"
 
 #Run the application
 $DIR/venv/bin/gunicorn -w 4 -b 0.0.0.0:$PORT app:app
+
