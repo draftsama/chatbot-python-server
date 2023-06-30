65b62f76999645284e454295662e9cebd29ff5d1
WIjarn Bangmo <draft.sama@gmail.com>
2023-06-30 14:00:28 +0700

    [update] start script

--- start.sh
+++ start.sh
@@ -2,9 +2,15 @@
 
 export OPENAI_API_KEY=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
 
-# DIR=$(dirname "$0")
+DIR=$(dirname "$0")
 
 export MODE=production
-$DIR/venv/bin/gunicorn -w 4 -b 0.0.0.0:3000 app:app
 
+# Change directory to project directory
+cd $DIR 
 
+# Terminate existing Gunicorn process
+pkill -f "$DIR/venv/bin/gunicorn -w 4 -b 0.0.0.0:$PORT app:app"
+
+#Run the application
+$DIR/venv/bin/gunicorn -w 4 -b 0.0.0.0:$PORT app:app
