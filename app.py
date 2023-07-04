5a50701008ea8eb86f8eb6c92332dfcb3ef48fdd
WIjarn Bangmo <draft.sama@gmail.com>
2023-07-04 15:38:41 +0700

    [fixed] issue

--- app.py
+++ app.py
@@ -291,10 +291,9 @@ def handle_message(event):
     }
 
     requests.post(url, headers=headers, json=data)
-    json_data = json.loads(event)
 
     # Convert back to a pretty-printed JSON string
-    pretty_json_string = json.dumps(json_data, indent=4)
+    pretty_json_string = json.dumps(event, indent=4)
     app.logger.info(f"user:\n{pretty_json_string}")
 
     if len(re.findall("ค้นหาสินค้า", event.message.text)) != 0:
