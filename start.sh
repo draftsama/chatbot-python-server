97f4280e46181751c95fda8e4dc3a198b3593176
WIjarn Bangmo <draft.sama@gmail.com>
2023-07-14 16:19:02 +0700

    [update]

--- start.sh
+++ start.sh
@@ -3,12 +3,15 @@
 DIR=$(dirname "$0")
 export MODE=production
 export PORT=3000
+export CUDA_VISIBLE_DEVICES="" 
 
 # Change directory to project directory
 cd $DIR 
 
 # Terminate existing Gunicorn process
 pkill -f "$DIR/venv/bin/gunicorn -w 4 -b 0.0.0.0:$PORT app:app"
+
+# Activate the virtual environment
 
 #Run the application
 $DIR/venv/bin/gunicorn -w 4 -b 0.0.0.0:$PORT app:app
