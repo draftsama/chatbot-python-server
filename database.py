37c545e16f3b70a144e055383091a2ad368d0612
Draft Sama <draft.sama@gmail.com>
2024-01-12 15:14:58 +0700

    [update] update function

--- database.py
+++ database.py
@@ -267,7 +267,7 @@ class DatabaseConnect:
         cur = conn.cursor()
         
          #get all column names without create_at and id
-        query = f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table}"
+        query = f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table}'"
         cur.execute(query)
         
         column_names = cur.fetchall()
